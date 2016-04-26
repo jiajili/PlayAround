8ec16dd7cf15f763a90dba7d79f5f43fb78721fb
jiajili <iamjiaji.li@gmail.com>
2016-04-26 15:09:27 -0700

    IQ009

---
+++ IQ/IQ_009.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayAround.IQ
+{
+    /// <summary>
+    /// https://leetcode.com/problems/palindrome-number/
+    /// Palindrome Number
+    /// Determine whether an integer is a palindrome. Do this without extra space.
+    /// </summary>
+    public class IQ_009
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x == int.MinValue) return false;
+
+            long y = Math.Abs(x);
+            long k = 0;
+
+            while (y > 0)
+            {
+                k *= 10;
+                k += (y % 10);
+                y /= 10;
+            }
+
+            if (k > int.MaxValue) return false;
+
+            return x == (int)k ? true : false;
+        }
+    }
+}
