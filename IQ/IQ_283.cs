3d900b298e528c9ddf9bad818fef22b810cb9fb3
jiajili <iamjiaji.li@gmail.com>
2016-05-19 06:18:05 -0700

    IQ283

---
+++ IQ/IQ_283.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayAround.IQ
+{
+    /// <summary>
+    /// 
+    /// Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of 
+    /// the non-zero elements.
+    /// For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+    /// Note:
+    /// You must do this in-place without making a copy of the array.
+    /// Minimize the total number of operations.
+    /// </summary>
+    public class IQ_283
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            if (nums == null || nums.Length <= 1) return;
+
+            int currentIndex = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[currentIndex++] = nums[i];
+                }
+            }
+
+            for (int i = currentIndex; i < nums.Length; i++)
+            {
+                nums[i] = 0;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PlayAround.IQ;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,16 @@ namespace PlayAround
     {
         static void Main(string[] args)
         {
+            IQ_283 test = new IQ_283();
+            int[] input = {0,1,0,3,12};
+            test.MoveZeroes(input);
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.WriteLine(input[i]);
+            }
+
+            Console.ReadKey();
         }
     }
 }
