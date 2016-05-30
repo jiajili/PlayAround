c6c0e0938b685afcd0bb28c5e5ee506347f777c4
jiajili <iamjiaji.li@gmail.com>
2016-05-30 13:38:53 -0700

    IQ105

---
+++ IQ/IQ_105.cs
@@ -0,0 +1,53 @@
+﻿using PlayAround.Common;
+using System;
+
+namespace PlayAround.IQ
+{
+    public class IQ_105
+    {
+        public TreeNode BuildTree(int[] preorder, int[] inorder)
+        {
+            if (preorder == null || preorder.Length == 0 || inorder == null || inorder.Length != preorder.Length) return null;
+
+            int preStart = 0;
+            int preEnd = preorder.Length - 1;
+            int inStart = 0;
+            int inEnd = inorder.Length - 1;
+
+            return this.Construct(preorder, preStart, preEnd, inorder, inStart, inEnd);
+        }
+
+        public TreeNode Construct(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd)
+        {
+            if (preStart > preEnd || inStart > inEnd)
+            {
+                return null;
+            }
+
+            int val = preorder[preStart];
+            TreeNode p = new TreeNode(val);
+
+            // try using BinarySearch to find the element?
+            //find parent element index from inorder
+            //int k = Array.BinarySearch(inorder, val);
+
+            // or putting all inorder elements into a Dictionary in order to find an element quickly O(1)?
+
+            //find parent element index from inorder
+            int k = 0;
+            for (int i = 0; i < inorder.Length; i++)
+            {
+                if (val == inorder[i])
+                {
+                    k = i;
+                    break;
+                }
+            }
+           
+            p.left = Construct(preorder, preStart + 1, preStart + (k - inStart), inorder, inStart, k - 1);
+            p.right = Construct(preorder, preStart + (k - inStart) + 1, preEnd, inorder, k + 1, inEnd);
+
+            return p;
+        }
+    }
+}
---
+++ PlayAroundTest/IQ/IQ105_UnitTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using PlayAround.IQ;
+
+namespace PlayAroundTest.IQ
+{
+    [TestClass]
+    public class IQ105_UnitTest
+    {
+        [TestMethod]
+        public void TestCase1()
+        {
+            int[] preorder = { 10, 8, 6, 9, 22, 11, 33 };
+            int[] inorder = { 6, 8, 9, 10, 11, 22, 33 };
+            
+            IQ_105 test = new IQ_105();
+            var treenode = test.BuildTree(preorder, inorder);
+            Assert.AreEqual(treenode.val, 10);
+            Assert.AreEqual(treenode.left.val, 8);
+            Assert.AreEqual(treenode.left.right.val, 9);
+            Assert.AreEqual(treenode.right.val, 22);
+        }
+
+        [TestMethod]
+        public void TestCase2()
+        {
+            int[] preorder = { 1,2 };
+            int[] inorder = { 2,1 };
+            IQ_105 test = new IQ_105();
+            var treenode = test.BuildTree(preorder, inorder);
+            
+        }
+    }
+}
