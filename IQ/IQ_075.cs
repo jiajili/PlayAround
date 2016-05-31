b39473c75ebd3d3f652c57b682ee4a55da49d399
jiajili <iamjiaji.li@gmail.com>
2016-05-30 22:18:40 -0700

    IQ075

---
+++ IQ/IQ_075.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayAround.IQ
+{
+    /// <summary>
+    /// https://leetcode.com/problems/sort-colors/
+    /// 
+    /// Given an array with n objects colored red, white or blue, sort them so that objects of the same color are adjacent, 
+    /// with the colors in the order red, white and blue.
+    /// Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+    /// </summary>
+    public class IQ_075
+    {
+        public void SortColors(int[] nums)
+        {
+            if (nums == null || nums.Length <= 1) return;
+
+            int red=0, white = 0, blue = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    nums[blue] = 2;
+                    nums[white] = 1;
+                    nums[red] = 0;
+                    blue++;
+                    white++;
+                    red++;
+                }
+                else if (nums[i] == 1)
+                {
+                    nums[blue] = 2;
+                    nums[white] = 1;
+                    blue++;
+                    white++;
+                }
+                else if (nums[i] == 2)
+                {
+                    nums[blue] = 2;
+                    blue++;
+                }
+            }
+            
+        }
+
+        public void SortColors2(int[] nums)
+        {
+            if (nums == null || nums.Length <= 1) return;
+
+            int red = 0, white = 0, blue = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    red++;
+                }
+                else if (nums[i] == 1)
+                {
+                    white++;
+                }
+                else if (nums[i] == 2)
+                {
+                    blue++;
+                }
+            }
+
+            int index = 0;
+
+            for (; index< red; index++)
+            {
+                nums[index] = 0;
+            }
+
+            for (; index < white + red; index++)
+            {
+                nums[index] = 1;
+            }
+
+            for (; index < white + red + blue; index++)
+            {
+                nums[index] = 2;
+            }
+
+        }
+    }
+}
--- IQ/IQ_105.cs
+++ IQ/IQ_105.cs
@@ -3,6 +3,14 @@ using System;
 
 namespace PlayAround.IQ
 {
+    /// <summary>
+    /// https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+    /// 
+    /// 105. Construct Binary Tree from Preorder and Inorder Traversal
+    /// Given preorder and inorder traversal of a tree, construct the binary tree.
+    /// Note:
+    /// You may assume that duplicates do not exist in the tree.
+    /// </summary>
     public class IQ_105
     {
         public TreeNode BuildTree(int[] preorder, int[] inorder)
