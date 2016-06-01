fd6ece8956b284af954993ed7fd467bc3a313f96
jiajili <iamjiaji.li@gmail.com>
2016-06-01 15:38:34 -0700

    IQ238

---
+++ IQ/IQ_238.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayAround.IQ
+{
+    /// <summary>
+    /// https://leetcode.com/problems/product-of-array-except-self/
+    /// 
+    /// Product of Array Except Self
+    /// Given an array of n integers where n > 1, nums, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+    /// Solve it without division and in O(n). 
+    /// For example, given [1,2,3,4], return [24,12,8,6].
+    /// </summary>
+    public class IQ_238
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            if (null == nums) return null;
+            int[] ret = new int[nums.Length];
+
+            int left = 1;
+            int right = 1;
+
+            int length = nums.Length;
+            for (int i = 0; i < length; i++)
+            {
+                ret[i] = 1;
+            }
+
+            for (int i = 0; i < length; i++)
+            {
+                ret[i] *= left;
+                ret[length - 1 - i] *= right;
+                left *= nums[i];
+                right *= nums[length - i - 1];
+            }
+
+            return ret;
+        }
+    }
+}
