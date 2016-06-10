da3a1573111545b30d6be33864e61639aa844c5d
jiajili <iamjiaji.li@gmail.com>
2016-06-10 06:23:45 -0700

    IQ238

---
+++ IQ/IQ_206.cs
@@ -0,0 +1,35 @@
+﻿using PlayAround.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayAround.IQ
+{
+    /// <summary>
+    /// https://leetcode.com/problems/reverse-linked-list/
+    /// 206. Reverse Linked List
+    /// 
+    /// Reverse a singly linked list.
+    /// </summary>
+    public class IQ_206
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null || head.next == null) return head;
+
+            ListNode prev = null,next = null;
+            ListNode current = head;
+
+            while (current != null)
+            {
+                next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+            return null;
+        }
+    }
+}
