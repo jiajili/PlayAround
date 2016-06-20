9ef1c6b2d5f038d696ffe8f043b083265257f2fb
jiajili <iamjiaji.li@gmail.com>
2016-06-20 15:26:35 -0700

    IQ234, IQ237

---
+++ IQ/IQ_234.cs
@@ -0,0 +1,56 @@
+﻿using PlayAround.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayAround.IQ
+{
+    class IQ_234
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            if (head == null || head.next == null) return true;
+
+            ListNode faster = head;
+            ListNode slower = head;
+
+            while (faster.next != null && faster.next.next != null)
+            {
+                faster = faster.next.next;
+                slower = slower.next;
+            }
+
+            ListNode middle = slower.next;
+            slower.next = null;
+
+            ListNode prev = null;
+            ListNode current = middle;
+            ListNode next = null;
+
+            while (current != null)
+            {
+                next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+
+            ListNode left = head;
+            ListNode right = prev;
+
+            while (left != null && right != null)
+            {
+                if (left.val != right.val)
+                {
+                    return false;
+                }
+                left = left.next;
+                right = right.next;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ IQ/IQ_237.cs
@@ -0,0 +1,20 @@
+﻿using PlayAround.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayAround.IQ
+{
+    class IQ_237
+    {
+        public void DeleteNode(ListNode node)
+        {
+            if (node == null || node.next == null) return;
+
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
