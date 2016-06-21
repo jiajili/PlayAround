413337e8ff9c552a3d90423100b5bf00290e70cf
jiajili <iamjiaji.li@gmail.com>
2016-06-21 10:21:39 -0700

    IQ350

---
+++ IQ/IQ_350.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayAround.IQ
+{
+    class IQ_350
+    {
+        public int[] Intersect(int[] nums1, int[] nums2)
+        {
+            Array.Sort(nums1);
+            Array.Sort(nums2);
+
+            int p1 = 0; int p2 = 0;
+
+            List<int> list = new List<int>();
+
+            while (p1 < nums1.Count() && p2 < nums2.Count())
+            {
+                if (nums1[p1] == nums2[p2])
+                {
+                    list.Add(nums1[p1]);
+                    p1++;
+                    p2++;
+                }
+                else if (nums1[p1] > nums2[p2])
+                {
+                    p2++;
+                }
+                else
+                {
+                    p1++;
+                }
+            }
+
+            return list.ToArray();
+        }
+    }
+}
