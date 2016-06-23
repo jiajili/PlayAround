0082423285b9904e6ad0575537ccedaceff5a5d0
jiajili <iamjiaji.li@gmail.com>
2016-06-23 09:23:00 -0700

    IQ171

---
+++ IQ/IQ_171.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayAround.IQ
+{
+    class IQ_171
+    {
+        public int TitleToNumber(string s)
+        {
+            long ret = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                ret *= 26;
+                ret += (s[i] - 'A' + 1);
+            }
+
+            if (ret > int.MaxValue)
+            {
+                return int.MaxValue;
+            }
+
+            return (int)ret;
+        }
+    }
+}
