a714ecaf11f02902939ba8d8cabd1efb5c69c23b
jiajili <iamjiaji.li@gmail.com>
2016-06-23 09:49:39 -0700

    IQ169

---
+++ IQ/IQ_169.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayAround.IQ
+{
+    class IQ_169
+    {
+        public int MajorityElement(int[] nums)
+        {
+            int currentValue = nums[0];
+            int count = 1;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (currentValue == nums[i])
+                {
+                    count++;
+                }
+                else
+                {
+                    if (count > 1)
+                    {
+                        count--;
+                    }
+                    else
+                    {
+                        count = 1;
+                        currentValue = nums[i];
+                    }
+                }
+            }
+
+            return currentValue;
+        }
+    }
+}
