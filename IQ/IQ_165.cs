e6e051d903767663db636e1d1ed858bb4cb652ca
jiajili <iamjiaji.li@gmail.com>
2016-06-23 07:41:55 -0700

    IQ165

---
+++ IQ/IQ_165.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayAround.IQ
+{
+    class IQ_165
+    {
+        public int CompareVersion(string version1, string version2)
+        {
+            string[] version1Arr = version1.Split('.');
+            string[] version2Arr = version2.Split('.');
+
+            int index = 0;
+
+            while (index < version1Arr.Length || index < version2Arr.Length)
+            {
+                if (index < version1Arr.Length && index < version2Arr.Length)
+                {
+                    if (int.Parse(version1Arr[index]) > int.Parse(version2Arr[index]))
+                    {
+                        return 1;
+                    }
+                    else if (int.Parse(version1Arr[index]) < int.Parse(version2Arr[index]))
+                    {
+                        return -1;
+                    }
+                }
+                else if (index >= version1Arr.Length)
+                {
+                    if(int.Parse(version2Arr[index]) > 0){
+                        return -1;
+                    }
+                }else if(index >= version2Arr.Length){
+                    if(int.Parse(version1Arr[index]) > 0){
+                        return 1;
+                    }
+                }
+
+                index++;
+            }
+
+            return 0;
+        }
+    }
+}
