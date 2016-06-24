5188858e9bd54849b50ca4af6bdc73adf1baeee7
jiajili <iamjiaji.li@gmail.com>
2016-06-24 06:56:40 -0700

    IQ203

---
+++ IQ/IQ_203.cs
@@ -0,0 +1,35 @@
+﻿using PlayAround.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayAround.IQ
+{
+    class IQ_203
+    {
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            if (null == head) return head;
+
+            ListNode previous = new ListNode(-1);
+            previous.next = head;
+
+            ListNode current = previous;
+            ListNode next = current.next;
+
+            while (next != null)
+            {
+                if (next.val != val)
+                {
+                    current.next = next;
+                    current = next;
+                }
+                    next = next.next;                
+            }
+
+            return previous.next;
+        }
+    }
+}
