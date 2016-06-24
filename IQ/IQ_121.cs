13c0c2eed06a39456f388db8a0f3853cd1f4e72d
jiajili <iamjiaji.li@gmail.com>
2016-06-24 11:54:17 -0700

    IQ121

---
+++ IQ/IQ_121.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayAround.IQ
+{
+    class IQ_121
+    {
+        public int MaxProfit(int[] prices)
+        {
+            if ((prices == null) || prices.Length < 2) return 0;
+            int maxProfit = 0;
+            int currentMin = prices[0];
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                maxProfit = Math.Max(maxProfit, prices[i] - currentMin);
+                currentMin = Math.Min(prices[i], currentMin);
+            }
+
+            return maxProfit;
+        }
+    }
+}
