a9f4029969bfc408296791cc83b07ea2dd993c07
jiajili <iamjiaji.li@gmail.com>
2016-06-24 11:08:24 -0700

    IQ226,IQ326

--- IQ/IQ_226.cs
+++ IQ/IQ_226.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PlayAround.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,32 @@ namespace PlayAround.IQ
 {
     class IQ_226
     {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null) return root;
+
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                TreeNode node = queue.Dequeue();
+                if (node.left != null)
+                {
+                    queue.Enqueue(node.left);
+                }
+
+                if (node.right != null)
+                {
+                    queue.Enqueue(node.right);
+                }
+
+                var tempNode = node.left;
+                node.left = node.right;
+                node.right = tempNode;
+            }
+
+            return root;
+        }
     }
 }
--- IQ/IQ_326.cs
+++ IQ/IQ_326.cs
@@ -8,5 +8,23 @@ namespace PlayAround.IQ
 {
     class IQ_326
     {
+        public bool IsPowerOfThree(int n)
+        {
+            if (n == 0) return false;
+            return n == Math.Pow(3, Math.Round(Math.Log(n) / Math.Log(3)));
+        }
+
+        public bool IsPowerOfThree2(int n)
+        {
+            if (n == 0) return false;
+            if (n == 1) return true;
+            if (n > 1)
+            {
+                return n % 3 == 0 && IsPowerOfThree2(n / 3);
+            }
+            else
+                return false;
+
+        }
     }
 }
